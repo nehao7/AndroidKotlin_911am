8a6bea9a6901e5db99eb0edffde816040a2c9f8c
Neha <neha@o7tec.com>
2025-01-06 15:20:57 +0530

    Activity Lifecycle

--- app/src/main/java/com/o7services/androidkotlin_9_11am/MainActivity.kt
+++ app/src/main/java/com/o7services/androidkotlin_9_11am/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.o7services.androidkotlin_9_11am
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
@@ -17,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show()
         btn=findViewById(R.id.btnWelcome)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -25,7 +28,42 @@ class MainActivity : AppCompatActivity() {
         }
 
         btn?.setOnClickListener {
-            Toast.makeText(this,"Welcome to my app",Toast.LENGTH_SHORT).show()
+            var intent=Intent(this, SecondActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+           Toast.makeText(this,"Welcome To my App",Toast.LENGTH_SHORT).show()
         }
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "OnRestart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show()
+    }
+
 }
--- app/src/main/java/com/o7services/androidkotlin_9_11am/SecondActivity.kt
+++ app/src/main/java/com/o7services/androidkotlin_9_11am/SecondActivity.kt
@@ -1,6 +1,8 @@
 package com.o7services.androidkotlin_9_11am
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,14 @@ class SecondActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnBack = findViewById<Button>(R.id.btnBack)
+
+        btnBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+        }
+
     }
 }
